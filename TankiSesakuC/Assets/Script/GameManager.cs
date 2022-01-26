f9b5b02b25e07778bcd194d1dde1028519d7efbf
HIBIKI-FURUGEN <hibiki-furugen-20f@stu.kbc.ac.jp>
2022-01-26 10:31:06 +0900

    abc

---
+++ TankiSesakuC/Assets/Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        Time.timeScale = 1f;//戻る
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
