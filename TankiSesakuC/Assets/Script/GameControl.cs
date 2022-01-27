53e35f6ca69472b6f001d1eb34f7fd167a40c689
KBC12005008\kbc12005008 <ryouichi-nakama-20f@stu.kbc.ac.jp>
2022-01-27 09:44:06 +0900

    no message

--- TankiSesakuC/Assets/Script/GameControl.cs
+++ TankiSesakuC/Assets/Script/GameControl.cs
@@ -21,8 +21,8 @@ public class GameControl : MonoBehaviour
         wpanel.gameObject.SetActive(false);
         lpanel = GameObject.Find("Lose");
         lpanel.gameObject.SetActive(false);
-        rlife = 3;
-        rgauge = 150;
+        rlife = 0;
+        rgauge = 200;
     }
 
     // Update is called once per frame
@@ -45,6 +45,20 @@ public class GameControl : MonoBehaviour
             else if (Input.GetButtonDown("Retry") && flag == 1)
             {
                 wpanel.gameObject.SetActive(false);
+                lpanel.gameObject.SetActive(false);
+                rpanel.gameObject.SetActive(true);
+                flag = 2;
+            }
+        }
+        if (rlife == 0)
+        {
+            if (Input.GetButtonDown("Retry") && flag == 0)
+            {
+                lpanel.gameObject.SetActive(true);
+                flag = 1;
+            }
+            else if (Input.GetButtonDown("Retry") && flag == 1)
+            {
                 lpanel.gameObject.SetActive(false);
                 rpanel.gameObject.SetActive(true);
                 flag = 2;
