f12ebd8cbb708e0a1e641cd39d5a986e0436c549
DESKTOP-F6TLTDN\hk416 <syoei-hanashiro-20f@stu.kbc.ac.jp>
2022-01-27 12:13:10 +0900

    板書の音

--- TankiSesakuC/Assets/Scripts/enemy.cs
+++ TankiSesakuC/Assets/Scripts/enemy.cs
@@ -1,22 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 //5秒ごとに振り向く　振り向き継続は1秒　振り向き継続が終了したらまた5秒計測する
 public class enemy : MonoBehaviour
 {
+    AudioSource audioSource;
     public Sprite bansyo, furimuki, tirami;
     public SpriteRenderer spriteRenderer;
     float turnTime = 0;
     float spanTime = 0;
     int time = 1, flame = 0, Feint = 0;
     public bool Turnflg = false;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine("Turn");
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -43,6 +47,7 @@ public class enemy : MonoBehaviour
         {
             yield return new WaitForSeconds(Random.Range(1,8));
             spriteRenderer.sprite = tirami;
+            audioSource.Stop();
             Feint = Random.Range(1, 10);
 
             if(Feint != 1)
@@ -55,6 +60,7 @@ public class enemy : MonoBehaviour
             yield return new WaitForSeconds(1);
             Turnflg = false;
             spriteRenderer.sprite = bansyo;
+            audioSource.Play();
         }
     }
 }
